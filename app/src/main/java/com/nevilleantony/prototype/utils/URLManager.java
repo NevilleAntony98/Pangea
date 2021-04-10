f5e0c57d83dd1b76fe5e23d3e16d917b883c00bc
Neville <25197458+NevilleAntony98@users.noreply.github.com>
2021-04-10 12:33:05 +0530

    URLManager: Refactor getProperties()
    Previously we checked if "accept-ranges" header was present, but some
    serves may not always include it. So instead a catch all solution
    would be to request a 1 byte range, and if it supports range request
    it would respond with a 206 or else a 200.

--- app/src/main/java/com/nevilleantony/prototype/utils/URLManager.java
+++ app/src/main/java/com/nevilleantony/prototype/utils/URLManager.java
@@ -26,33 +26,70 @@ public class URLManager {
 		HttpURLConnection httpURLConnection = null;
 		boolean isReachable = false;
 		boolean canAcceptRanges = false;
+		long contentLength = 0;
 
 		try {
 			URL url = new URL(urlString);
 			httpURLConnection = (HttpURLConnection) url.openConnection();
-			isReachable = httpURLConnection.getResponseCode() == 200;
-			if (isReachable) {
-				String acceptRangesField = httpURLConnection.getHeaderField("Accept-Ranges");
-				canAcceptRanges = acceptRangesField != null && acceptRangesField.equals("bytes");
+			httpURLConnection.setRequestProperty("Range", "bytes=0-0");
+
+			int responseCode = httpURLConnection.getResponseCode();
+			if (responseCode == HttpURLConnection.HTTP_OK) {
+				isReachable = true;
+
+				String contentLengthField = httpURLConnection.getHeaderField("content-length");
+				if (contentLengthField != null) {
+					contentLength = Long.parseLong(contentLengthField);
+				}
 			}
+
+			if (responseCode == HttpURLConnection.HTTP_PARTIAL) {
+				isReachable = true;
+				canAcceptRanges = true;
+
+				String contentRangeField = httpURLConnection.getHeaderField("content-range");
+				if (contentRangeField != null ) {
+					String size = contentRangeField.split("/")[1];
+					if (!size.equals("*")) {
+						contentLength = Long.parseLong(size);
+						Log.d(TAG, "getProperties: size: " + Utils.getHumanReadableSize(contentLength));
+					}
+				}
+			}
+
 		} catch (IOException e) {
 			Log.d(TAG, "getURLProperties: Failed URL: " + urlString);
 			e.printStackTrace();
 		} finally {
-			if (httpURLConnection != null)
+			if (httpURLConnection != null) {
 				httpURLConnection.disconnect();
+			}
 		}
 
-		return new URLProperties(isReachable, canAcceptRanges);
+		return new URLProperties(isReachable, canAcceptRanges)
+				.setContentLength(contentLength);
 	}
 
 	public static class URLProperties {
 		public final boolean isReachable;
 		public final boolean canAcceptRanges;
+		private long contentLength;
 
 		public URLProperties(boolean isReachable, boolean canAcceptRanges) {
 			this.isReachable = isReachable;
 			this.canAcceptRanges = canAcceptRanges;
+			contentLength = 0;
+		}
+
+		public URLProperties from(URLProperties urlProperties) {
+			return new URLProperties(urlProperties.isReachable, urlProperties.canAcceptRanges)
+					.setContentLength(urlProperties.contentLength);
+		}
+
+		public URLProperties setContentLength(long contentLength) {
+			this.contentLength = contentLength;
+
+			return this;
 		}
 	}
 }
