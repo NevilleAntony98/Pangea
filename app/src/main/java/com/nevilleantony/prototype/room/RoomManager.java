df5e44c1cdce7ab3ac5e3432956eeab5f9e7c1cd
Neville <25197458+NevilleAntony98@users.noreply.github.com>
2021-04-18 17:58:42 +0530

    RoomManager: Add abstract RoomManager
    This class will be extended by atleast 2 implementations:
        - A legacy implementation using createGroup()
        - A wifi direct implementation using connect()
    
    The former creates a room with current device as the group owner
    and the latter decides the group owner based on battey power, signal
    strength etc.
    
    For now, legacy implementation will have higher priority so that we
    can assign the group owner ourselves.

---
+++ app/src/main/java/com/nevilleantony/prototype/room/RoomManager.java
@@ -0,0 +1,78 @@
+package com.nevilleantony.prototype.room;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.os.Looper;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.nevilleantony.prototype.peer.Peer;
+
+public abstract class RoomManager {
+	public static final boolean LEGACY = true;
+	private static final String TAG = "RoomManager";
+	private static RoomManager roomManager = null;
+
+	public final IntentFilter intentFilter;
+	public final WifiP2pManager manager;
+	public final WifiP2pManager.Channel channel;
+
+	protected RoomManager(Context context) {
+		intentFilter = setupIntentFilter();
+		manager = (WifiP2pManager) context.getSystemService(Context.WIFI_P2P_SERVICE);
+		channel = manager.initialize(context, Looper.getMainLooper(), null);
+	}
+
+	public static RoomManager getInstance(Context context) {
+		if (roomManager == null) {
+			if (LEGACY) {
+				roomManager = new LegacyRoomManager(context);
+			} else {
+				roomManager = new P2pRoomManager(context);
+			}
+		}
+
+		return roomManager;
+	}
+
+	private static IntentFilter setupIntentFilter() {
+		IntentFilter intentFilter = new IntentFilter();
+		// Indicates a change in the Wi-Fi P2P status.
+		intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+		// Indicates a change in the list of available peers.
+		intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+		// Indicates the state of Wi-Fi P2P connectivity has changed.
+		intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+		// Indicates this device's details have changed.
+		intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+		return intentFilter;
+	}
+
+	public void initiateDiscovery(Context context, WifiP2pManager.ActionListener actionListener) {
+		if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+			Log.e(TAG, "initiateDiscovery: How did you even reach here?");
+
+			return;
+		}
+
+		manager.discoverPeers(channel, actionListener);
+	}
+
+	public abstract void connect(AppCompatActivity context, Peer peer);
+
+	public void requestPeers(Context context, WifiP2pManager.PeerListListener listener) {
+		if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+			Log.e(TAG, "requestPeers: How did you even reach here?");
+
+			return;
+		}
+
+		manager.requestPeers(channel, listener);
+	}
+}
