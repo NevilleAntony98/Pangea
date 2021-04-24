4d06d3a7900172417148002e7b9503d3b6e254d0
Neville <25197458+NevilleAntony98@users.noreply.github.com>
2021-04-24 13:47:11 +0530

    Peer: Add some static helper functions

--- app/src/main/java/com/nevilleantony/prototype/peer/Peer.java
+++ app/src/main/java/com/nevilleantony/prototype/peer/Peer.java
@@ -1,7 +1,12 @@
 package com.nevilleantony.prototype.peer;
 
 import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 public abstract class Peer {
@@ -40,4 +45,44 @@ public abstract class Peer {
 	}
 
 	abstract public PeerType getType();
+
+	public static List<Peer> getPeerList(Collection<WifiP2pDevice> deviceList) {
+		List<Peer> peers = new ArrayList<>();
+		for (WifiP2pDevice device : deviceList) {
+			if (device.isGroupOwner()) {
+				peers.add(new AdminPeer(device));
+			} else {
+				peers.add(new DefaultPeer(device));
+			}
+		}
+
+		return peers;
+	}
+
+	public static List<Peer> getPeerList(WifiP2pDeviceList deviceList) {
+		List<Peer> peers = new ArrayList<>();
+		for (WifiP2pDevice device : deviceList.getDeviceList()) {
+			if (device.isGroupOwner()) {
+				peers.add(new AdminPeer(device));
+			} else {
+				peers.add(new DefaultPeer(device));
+			}
+		}
+
+		return peers;
+	}
+
+	public static void logPeerList(WifiP2pDeviceList deviceList) {
+		String TAG = "PeerLogger";
+		for (WifiP2pDevice device: deviceList.getDeviceList()) {
+			Log.d(TAG, "peer: " + device.deviceName);
+		}
+	}
+
+	public static void logPeerList(Collection<WifiP2pDevice> deviceList) {
+		String TAG = "PeerLogger";
+		for (WifiP2pDevice device: deviceList) {
+			Log.d(TAG, "peer: " + device.deviceName);
+		}
+	}
 }
