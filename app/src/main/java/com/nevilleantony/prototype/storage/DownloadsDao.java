ff61aa234ebf2425cde4e7f62ff5e1efeba04b76
kss682 <kss682@gmail.com>
2021-04-28 15:24:14 +0530

    Build: Add rxjava3 dependency

--- app/src/main/java/com/nevilleantony/prototype/storage/DownloadsDao.java
+++ app/src/main/java/com/nevilleantony/prototype/storage/DownloadsDao.java
@@ -3,38 +3,50 @@ package com.nevilleantony.prototype.storage;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import java.util.List;
+
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Single;
 
 @Dao
 public interface DownloadsDao{
 
     @Query("SELECT * from downloads")
-    List<DownloadsModel> getAll();
+    Single<List<DownloadsModel>> getAll();
 
     @Query("SELECT id from downloads")
-    List<String> retrieveId();
+    Single<List<String>> retrieveId();
 
     @Query("SELECT range from downloads")
-    List<String> retrieveRange();
+    Single<List<Long>> retrieveRange();
 
     @Query("Select file_url from downloads where id = :groupId and range = :fileRange")
-    List<Long> retrieveFileUrl(String groupId, Long fileRange);
+    Single<List<String>> retrieveFileUrl(String groupId, Long fileRange);
+
+    @Query("Select file_name from downloads where id = :groupId and range = :fileRange")
+    Single<List<String>> retrieveFileName(String groupId, Long fileRange);
 
     @Query("Select min_range, max_range from downloads where id = :groupId and range = :fileRange")
-    List<Long> retrieveMinMaxRange(String groupId, Long fileRange);
+    Single<List<RangeTuple>> retrieveMinMaxRange(String groupId, Long fileRange);
 
-    @Query("update downloads set min_range =: minRange where id = :groupId and range = :Drange")
-    void updateMinRange(String groupId, Long fileRange, Long minRange);
+    @Query("update downloads set min_range = :minRange where id = :groupId and range = :fileRange")
+    Completable updateMinRange(String groupId, Long fileRange, Long minRange);
 
 
-    @Query("update downloads set max_range =: maxRange where id = :groupId and range = :fileRange")
-    void updateMaxRange(String groupId, Long fileRange, Long maxRange);
+    @Query("update downloads set max_range = :maxRange where id = :groupId and range = :fileRange")
+    Completable updateMaxRange(String groupId, Long fileRange, Long maxRange);
 
-    @Insert
-    void insertDownloads(DownloadsModel downloads);
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    Completable insertDownloads(DownloadsModel downloads);
 
     @Delete
-    void deleteDownloads(DownloadsModel downloads);
+    Completable deleteDownloads(DownloadsModel downloads);
 
+    class RangeTuple{
+        public Long min_range;
+        public Long max_range;
+    }
 }
+
--- app/src/main/java/com/nevilleantony/prototype/storage/DownloadsModel.java
+++ app/src/main/java/com/nevilleantony/prototype/storage/DownloadsModel.java
@@ -10,29 +10,42 @@ import androidx.room.Query;
 import java.util.List;
 
 //https://github.com/googlecodelabs/android-room-with-a-view/tree/master/app/src/main/java/com/example/android/roomwordssample
-@androidx.room.Entity(tableName = "downloads")
+@androidx.room.Entity(tableName = "downloads", primaryKeys = {"id", "range"})
 public class DownloadsModel {
     //hash of the file_url + timestamp
     /*
         The primary key has to be (id,range) as a user can be reassigned to download different range,
         so to uniquely identify a row we need id, range.
      */
-    @PrimaryKey
     @NonNull
     private String id;
     private String file_url;
-    @PrimaryKey
+    private String file_name;
+    @NonNull
     private Long range;
     private Long min_range;
     private Long max_range;
 
 
-    DownloadsModel(String id, String file_url, Long range, Long min_range, Long max_range){
+    DownloadsModel(@NonNull String id, String file_url,String file_name,@NonNull Long range, Long min_range, Long max_range){
         this.id = id;
         this.file_url = file_url;
+        this.file_name = file_name;
         this.range = range;
         this.min_range = min_range;
         this.max_range = max_range;
     }
+
+    /*
+    Getter and Setter methods can be added if needed
+     */
+    @NonNull
+    public String getId(){ return id;}
+    public String getFile_url(){ return file_url;}
+    public String getFile_name(){ return file_name;}
+    @NonNull
+    public  Long getRange(){ return range;}
+    public Long getMin_range(){ return min_range;}
+    public Long getMax_range(){ return max_range;}
 }
 
