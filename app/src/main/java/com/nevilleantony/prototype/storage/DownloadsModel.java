033cc49288ac07b95ce25922bd6002beaf019ed5
Atul71 <atula.mec@gmail.com>
2021-04-28 12:30:12 +0530

    Database: Add DownloadsDoa

--- app/src/main/java/com/nevilleantony/prototype/storage/DownloadsModel.java
+++ app/src/main/java/com/nevilleantony/prototype/storage/DownloadsModel.java
@@ -2,8 +2,12 @@ package com.nevilleantony.prototype.storage;
 
 import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
+import androidx.room.Dao;
 import androidx.room.PrimaryKey;
 import androidx.room.Entity;
+import androidx.room.Query;
+
+import java.util.List;
 
 //https://github.com/googlecodelabs/android-room-with-a-view/tree/master/app/src/main/java/com/example/android/roomwordssample
 @androidx.room.Entity(tableName = "downloads")
@@ -18,8 +22,17 @@ public class DownloadsModel {
     private String id;
     private String file_url;
     @PrimaryKey
-    private long range;
-    private long min_range;
-    private long max_range;
+    private Long range;
+    private Long min_range;
+    private Long max_range;
 
+
+    DownloadsModel(String id, String file_url, Long range, Long min_range, Long max_range){
+        this.id = id;
+        this.file_url = file_url;
+        this.range = range;
+        this.min_range = min_range;
+        this.max_range = max_range;
+    }
 }
+
