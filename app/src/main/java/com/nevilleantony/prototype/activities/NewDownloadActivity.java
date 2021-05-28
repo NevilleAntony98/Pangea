91c1d957acd8adb10e9502677e40de5c0a91babe
Neville <25197458+NevilleAntony98@users.noreply.github.com>
2021-05-28 13:46:40 +0530

    NewDownloadActivity: Refactor onClick
    Simplify it with a map instead of creating a new fragment each time.
    ALso check for location before allowing to view join subpage.

--- app/src/main/java/com/nevilleantony/prototype/activities/NewDownloadActivity.java
+++ app/src/main/java/com/nevilleantony/prototype/activities/NewDownloadActivity.java
@@ -1,46 +1,49 @@
 package com.nevilleantony.prototype.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.google.android.material.button.MaterialButtonToggleGroup;
 import com.nevilleantony.prototype.R;
-import com.nevilleantony.prototype.fragments.CreateOrJoinSubpage;
 import com.nevilleantony.prototype.fragments.CreateRoomSubpage;
 import com.nevilleantony.prototype.fragments.JoinRoomSubpage;
+import com.nevilleantony.prototype.utils.Utils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class NewDownloadActivity extends AppCompatActivity {
+
+	private Map<Integer, Fragment> fragmentsMap;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_download);
 
-		MaterialButtonToggleGroup buttonToggleGroup = findViewById(R.id.new_download_toggle_button_group);
-		buttonToggleGroup.addOnButtonCheckedListener(new MaterialButtonToggleGroup.OnButtonCheckedListener() {
-			@Override
-			public void onButtonChecked(MaterialButtonToggleGroup group, int checkedId, boolean isChecked) {
-				Fragment fragment;
-				switch (checkedId) {
-					case R.id.create_room_button:
-						fragment = new CreateRoomSubpage();
-						break;
-					case R.id.join_room_button:
-						fragment = new JoinRoomSubpage();
-						break;
-					default:
-						fragment = new CreateOrJoinSubpage();
-				}
+		fragmentsMap = new HashMap<>();
+		fragmentsMap.put(R.id.create_room_button, new CreateRoomSubpage());
+		fragmentsMap.put(R.id.join_room_button, new JoinRoomSubpage());
 
-				getSupportFragmentManager().beginTransaction()
-						.setReorderingAllowed(true)
-						.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-						.replace(R.id.new_download_subpage_container, fragment)
-						.commit();
-			}
-		});
+		Button createRoomButton = findViewById(R.id.create_room_button);
+		Button joinRoomButton = findViewById(R.id.join_room_button);
+		createRoomButton.setOnClickListener(this::onButtonClicked);
+		joinRoomButton.setOnClickListener(this::onButtonClicked);
+	}
+
+	private void onButtonClicked(View view) {
+		if (Utils.isLocationEnabled(this)) {
+			getSupportFragmentManager().beginTransaction()
+					.setReorderingAllowed(true)
+					.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+					.replace(R.id.new_download_subpage_container, fragmentsMap.get(view.getId()))
+					.commit();
+		} else {
+			Utils.tryRequestLocation(this);
+		}
 	}
 }
