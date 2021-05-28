2012f377a4dc4804fff516fd88b42a7065512c11
Neville <25197458+NevilleAntony98@users.noreply.github.com>
2021-05-28 13:43:58 +0530

    Utils: Add location helper functions

--- app/src/main/java/com/nevilleantony/prototype/utils/Utils.java
+++ app/src/main/java/com/nevilleantony/prototype/utils/Utils.java
@@ -1,8 +1,12 @@
 package com.nevilleantony.prototype.utils;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.location.LocationManager;
+import android.provider.Settings;
 
-import java.security.MessageDigest;
 import java.text.CharacterIterator;
 import java.text.StringCharacterIterator;
 import java.util.UUID;
@@ -26,5 +30,27 @@ public class Utils {
 
 	public static String getDigest(String data) {
 		return UUID.nameUUIDFromBytes(data.getBytes()).toString();
+	}
+
+	public static boolean isLocationEnabled(Context context) {
+		final LocationManager manager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+
+		return manager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+	}
+
+	public static void tryRequestLocation(Context context) {
+		if (!isLocationEnabled(context)) {
+			new AlertDialog.Builder(context)
+					.setMessage("Please enable location to continue.")
+					.setCancelable(false)
+					.setPositiveButton("Yes",
+							(dialog, which) -> {
+								context.startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+								dialog.cancel();
+							})
+					.setNegativeButton("No", (dialog, which) -> dialog.cancel())
+					.create()
+					.show();
+		}
 	}
 }
