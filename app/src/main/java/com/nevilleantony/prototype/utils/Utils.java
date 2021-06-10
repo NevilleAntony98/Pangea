348d762a3bd41bdbc882b27264ac2874c90d8c22
Neville <25197458+NevilleAntony98@users.noreply.github.com>
2021-06-10 20:05:19 +0530

    Utils: Add helper methods to for sharing wifi info

--- app/src/main/java/com/nevilleantony/prototype/utils/Utils.java
+++ app/src/main/java/com/nevilleantony/prototype/utils/Utils.java
@@ -5,11 +5,22 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.location.LocationManager;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
 import android.provider.Settings;
+import android.util.Log;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.text.CharacterIterator;
 import java.text.StringCharacterIterator;
+import java.util.StringTokenizer;
 import java.util.UUID;
+
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import kotlin.Pair;
 
 public class Utils {
 	@SuppressLint("DefaultLocale")
@@ -52,5 +63,26 @@ public class Utils {
 					.create()
 					.show();
 		}
+	}
+
+	public static String encodeWifi(WifiManager.LocalOnlyHotspotReservation reservation) {
+		WifiConfiguration wifiConfiguration = reservation.getWifiConfiguration();
+		String ssid = wifiConfiguration.SSID;
+		String passphrase = wifiConfiguration.preSharedKey;
+
+		return String.format("WIFI:S:%s;T:WPA;P:%s;;", ssid, passphrase);
+	}
+
+	public static Pair<String, String> getWifiInfo(String encoded) {
+		StringTokenizer tokens = new StringTokenizer(encoded, ":,;");
+		String WifiKey = tokens.nextToken();
+		String S = tokens.nextToken();
+		String ssid = tokens.nextToken();
+		String T = tokens.nextToken();
+		String auth = tokens.nextToken();
+		String P = tokens.nextToken();
+		String password = tokens.nextToken();
+
+		return new Pair<>(ssid, password);
 	}
 }
